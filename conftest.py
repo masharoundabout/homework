79787a8428d1fac84bcc8fc2351c766a21ae8258
masharoundabout <masharoundabout@Mashas-MacBook-Air.local>
2025-07-28 17:55:12 -0400

    change browser settings

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-
+    browser.driver.capabilities['enableVNC'] = True
     yield browser
 
     browser.quit()
