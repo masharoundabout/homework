06546e88bc0b76cbc237fafc8621e25514a839b5
masharoundabout <masharoundabout@Mashas-MacBook-Air.local>
2025-07-28 18:23:26 -0400

    changeorder in confest5

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.driver.capabilities['enableVNC'] = True
     browser.config.base_url = 'https://demoqa.com'
     yield browser
 
