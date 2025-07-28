3f64abf58025d5e3303fec1c70ed6282d0a7d49e
masharoundabout <masharoundabout@Mashas-MacBook-Air.local>
2025-07-28 18:38:07 -0400

    changeorder in confest7

--- conftest.py
+++ conftest.py
@@ -1,15 +1,32 @@
 import pytest
-from selene import browser
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.config.browser_name = 'chrome'
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"http://164.92.65.210:4444/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
     browser.config.base_url = 'https://demoqa.com'
     yield browser
 
     browser.quit()
-
 
 
 
