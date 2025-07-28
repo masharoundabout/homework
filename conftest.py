0b108798d30194a6fa326e1be4f326288178c3c1
masharoundabout <masharoundabout@Mashas-MacBook-Air.local>
2025-07-28 18:29:43 -0400

    changeorder in confest6

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selene import browser
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
+    browser.config.browser_name = 'chrome'
     browser.config.base_url = 'https://demoqa.com'
     yield browser
 
